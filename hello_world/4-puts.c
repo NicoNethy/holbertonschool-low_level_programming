20a8b376be1709b462be77ecd724f17f29007637
root <5761@holbertonstudents.com>
2022-09-27 07:13:22 -0700

    push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *puts
- *    * Return: Always 0 (Success)
- *     */
-int main (void) {
-       	puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+ *main - Entry point
+ *puts
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
