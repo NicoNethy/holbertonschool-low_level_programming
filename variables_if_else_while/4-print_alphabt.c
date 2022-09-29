2e7a441f769e8ccb6cf6cd8d6ed1be4a9f834798
root <5761@holbertonstudents.com>
2022-09-29 05:03:31 -0700

    supr

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	if (n != 'e' && n != 'q')
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
