01d384f6aef7ba84d026fcf242f6f6f37954e2b6
root <5761@holbertonstudents.com>
2022-09-29 07:16:20 -0700

    por

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * putchar
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (int x = 'a'; x <= 'z'; x++)
+	putchar(x);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,8 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 int _putchar(char c);
+void print_alphabet(void);
+
