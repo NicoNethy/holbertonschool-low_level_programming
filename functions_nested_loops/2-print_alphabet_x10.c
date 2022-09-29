b24eb313f75f7fb44e61fb81c168e67b0dcd0143
root <5761@holbertonstudents.com>
2022-09-29 08:59:12 -0700

    todo

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * _putchar
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char n;
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	_putchar(n);
+
+	}
+	putchar('\n');
+}}
