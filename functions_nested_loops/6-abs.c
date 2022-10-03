285a7818058ea1ad89b8eddc4ef33eadaeb02a0d
root <5761@holbertonstudents.com>
2022-10-03 05:20:59 -0700

    asd

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: input number
+ * Return: absolute value
+ */
+int _abs(int n)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+		else
+	{
+		return (n * -1);
+	}
+}
