eec63b4ef8bc38aba5cf125c13dec2cb207a229a
root <5761@holbertonstudents.com>
2022-10-04 06:48:10 -0700

    asd

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *  * print_numbers - Entry point
+ *   * _putchar
+ *    * Return: void
+ *     */
+
+void print_numbers(void);
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		{
+			_putchar(n);
+		}
+	putchar('\n');
+}
