20ebce6f4d8e84024ba88235b34e4656573e3ace
root <5761@holbertonstudents.com>
2022-10-05 05:20:54 -0700

    asd

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - Write a function that prints 10 times the numbers
+ * Return: numbers
+ */
+
+void more_numbers(void)
+
+{
+	int n, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');		
+	}
+}
