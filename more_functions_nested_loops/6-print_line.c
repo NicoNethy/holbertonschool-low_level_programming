a54203b87dc9c95cadbba041dc54b2089e14e742
root <5761@holbertonstudents.com>
2022-10-05 05:50:12 -0700

    asd

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  ** print_line - Write a function that draws a straigh line
+ *   ** @n: fisrt imput
+ *    ** Return: result
+ *     **/
+void print_line(int n)
+{
+		int n1;
+
+			if (n > 0)
+					{
+								for (n1 = 0; n1 < n; n1++)
+											_putchar('_');
+									}
+
+				_putchar('\n');
+}
