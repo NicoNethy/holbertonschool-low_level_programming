4282cfe347c7b83a3bb7076d89a50c22c947b164
root <5761@holbertonstudents.com>
2022-10-06 06:56:30 -0700

    asd

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - start point
+ * @s: the string
+ * Return: strlen
+ */
+
+int _strlen(char *s)
+
+{
+return (strlen(s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif 
