6aa50f18ddcc34540b8a2f10501f6d8243ad22ee
root <5761@holbertonstudents.com>
2022-10-06 07:17:45 -0700

    asd

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Start
+ * puts: it prints the string
+ * @str: is the string
+ * stdout
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	puts(str);
+		stdout;
+}
