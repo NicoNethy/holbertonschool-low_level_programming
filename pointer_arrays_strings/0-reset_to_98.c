e468c272f26387ac5f49b6f1e3ea3a5c362fc188
root <5761@holbertonstudents.com>
2022-10-06 06:16:28 -0700

    point

---
+++ pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98: starting point
+ * Pointer *p that points to int n
+ * n: is the int that gets pointed
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+
+{
+	int *p;
+
+	p = &*n;
+	*p = 98;
+
+}
+
---
+++ pointer_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+void reset_to_98(int *n);
+#endif 
