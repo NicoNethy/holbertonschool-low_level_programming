c7b321760158852f0d77807174eb024fdf536c92
root <5761@holbertonstudents.com>
2022-10-07 04:42:23 -0700

    asd

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- *  * puts2 - prints every other character of a  string to stdout
- *   * @str: string to print
- *    *
- *     * Return: void
- *      */
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+
 void puts2(char *str)
 {
+
 	int i = 0;
 
 	while (*(str + i))
