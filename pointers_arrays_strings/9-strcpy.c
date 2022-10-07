9443024bdb25ae6e8db10f7ce6f014a5f6e45643
root <5761@holbertonstudents.com>
2022-10-07 06:00:03 -0700

    asd

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - Start point
+ * @dest: destination of the code
+ * @src: source of the pointer
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	strcpy(dest, src);
+		return (dest);
+}
