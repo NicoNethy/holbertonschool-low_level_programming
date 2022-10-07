7e2e51d51eac90dcdf3a6085722fd3db871be657
root <5761@holbertonstudents.com>
2022-10-07 04:41:04 -0700

    asd

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *  * puts2 - prints every other character of a  string to stdout
@@ -12,8 +12,8 @@ void puts2(char *str)
 
 	while (*(str + i))
 	{
-		_putchar(*(str + i));
+		putchar(*(str + i));
 		i = i + 2;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
