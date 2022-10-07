c2d76125dc734990270e0112e157f8a68f902961
root <5761@holbertonstudents.com>
2022-10-07 05:23:53 -0700

    asd

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+
+{
+	int half;
+	int iensure = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		iensure += 1;
+	}
+	for (half = (_strlen(str) + iensure) / 2; half < _strlen(str); half++)
+	{
+		putchar(str[half]);
+	}
+	putchar('\n');
+}
