14dc9d3a0f6dce5053c5b92ee85bd115f090b906
root <5761@holbertonstudents.com>
2022-10-11 06:01:56 -0700

    ASd

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *leet - outstanding manouver
+ * *@str: string
+ * *Return: mozart needs a comeback tbh
+ * */
+
+char *leet(char *str)
+{
+	char theforbidden[] = "a4A4e3E3o0O0t7T7l1L1";
+	int very;
+	int ok;
+
+	for (very = 0; str[very] != '\0'; very++)
+	{
+		for (ok = 0; theforbidden[ok] != '\0'; ok++)
+		{
+			if (str[very] == theforbidden[ok])
+			{
+				str[very] = theforbidden[ok + 1];
+				break;
+			}
+		}
+	}
+	return (str);
+}
