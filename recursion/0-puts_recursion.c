1e72aa71ea10c9bd27dba2bf45bc3d49b62654dd
root <5761@holbertonstudents.com>
2022-10-14 06:31:55 -0700

    recursado

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
-/*
- *
- *
- *
- */
-
+/**
+ *_puts_recursion - puts with recursion
+ *@s: pointer
+ *Return: nanai
+ **/
 void _puts_recursion(char *s)
-
 {
 	if (*s == '\0')
 	{
