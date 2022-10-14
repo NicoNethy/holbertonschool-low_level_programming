23e56c80f4f8baea354bcfcb2e6fa30e33efd64d
root <5761@holbertonstudents.com>
2022-10-14 08:50:42 -0700

    asd

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -10,7 +10,7 @@
 int _pow_recursion(int x, int y)
 
 {
-	if (y < 0)	
+	if (y < 0)
 		return (-1);
 
 	if (x == 0)
@@ -19,6 +19,6 @@ int _pow_recursion(int x, int y)
 	if (x == 1 || y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 
 }
