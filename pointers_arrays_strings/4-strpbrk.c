32db4df0921ee25795c08a8c0e49b5c1d6baaab2
root <5761@holbertonstudents.com>
2022-10-14 05:28:34 -0700

    bark

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *_strpbrk - professional detective (again)
- * *@s: string
- * *@accept: Y/N
- * *Return: returns a char- https://www.youtube.com/watch?v=wyYgB0yolEI
- * */
+ * _strpbrk - professional detective (again)
+ * @s: string
+ * @accept: Y/N
+ * Return: returns a char- https://www.youtube.com/watch?v=wyYgB0yolEI
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
