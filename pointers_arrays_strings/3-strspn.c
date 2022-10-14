7376eaec752733e3088cb482aea6dbfab0c2e381
root <5761@holbertonstudents.com>
2022-10-14 05:25:53 -0700

    ay

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *_strspn - gets the length of a prefix substring. cool
- * *@s: pointer char
- * *@accept: Y/N
- * *Return: prefix rights
- * */
+ * _strspn - gets the length of a prefix substring. cool
+ * @s: pointer char
+ * @accept: Y/N
+ * Return: prefix rights
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
