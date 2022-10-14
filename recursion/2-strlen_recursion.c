2231541d872b8304f18b938756b664a798de346e
root <5761@holbertonstudents.com>
2022-10-14 07:30:31 -0700

    asd

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *_strlen_recursion - recursiona
+ * @s: the string
+ * Return: la longitud
  */
 
 int _strlen_recursion(char *s)
 
 {
-	return (strlen(s))
+	return (strlen(s));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -22,4 +22,5 @@ char *_strncpy(char *dest, char *src, int n);
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 #endif
