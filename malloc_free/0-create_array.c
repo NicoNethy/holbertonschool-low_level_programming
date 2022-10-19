ab362e34941ed2a42bda1e494210a9f22351133d
root <5761@holbertonstudents.com>
2022-10-19 06:37:56 -0700

    asd

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *b;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	b = malloc(size * sizeof(char));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < size; a++)
+	{
+		b[a] = c;
+	}
+	return (b);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,30 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+char *create_array(unsigned int size, char c);
+#endif
