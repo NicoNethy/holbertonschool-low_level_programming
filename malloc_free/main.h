10bd70e0f9898d826a3a8d963716e0d3881ee19a
root <5761@holbertonstudents.com>
2022-10-19 07:34:11 -0700

    asd

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -28,4 +28,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
