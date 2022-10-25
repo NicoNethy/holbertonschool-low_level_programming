476cbb67769e943140427401fb176a6f13bb8603
root <5761@holbertonstudents.com>
2022-10-25 07:03:26 -0700

    asd

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid previously created by alloc_grid function.
+ * @grid: grid to free
+ * @height: height of grid
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	ptr = malloc(nmemb);
+	if nmemb = '0'
+		return (NULL);
+	else if size = '0'
+		return (NULL);
+	else if (!ptr)
+		return (NULL);
+}
+	
