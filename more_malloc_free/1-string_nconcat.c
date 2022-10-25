3753e4c6502e5523a102b2aa40583a594919fcfb
root <5761@holbertonstudents.com>
2022-10-25 08:19:40 -0700

    asd

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * *string_nconcat - concatenates two strings
- * *@s1: string 1
- * *@s2: string 2
- * *@n: bytes necessary
- * *Return: pointer to the concatenated string
- * */
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes necessary
+ * Return: pointer to the concatenated string
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
