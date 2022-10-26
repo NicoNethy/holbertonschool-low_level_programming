c286394ff7eb4f40737d9a2595333a984f161124
root <5761@holbertonstudents.com>
2022-10-26 12:27:43 -0700

    asd

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the file
+ * Return: it was a triumph. im making a note here: "huge success"
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
