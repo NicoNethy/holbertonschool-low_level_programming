f62d1f844e568d369a324be75c152dbaa4a31008
root <5761@holbertonstudents.com>
2022-10-31 07:50:31 -0700

    asd

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ *  * struct dog - dog!
+ *   * @name: Lucky
+ *    * @age: 4
+ *     * @owner: Mauricio de Betolaza
+ *      */
+
+struct dog
+{
+		char *name;
+			float age;
+				char *owner;
+};
+
+typedef struct dog dog_t;
+
+void free_dog(dog_t *d);
+#endif
