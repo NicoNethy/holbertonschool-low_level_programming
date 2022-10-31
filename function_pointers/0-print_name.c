ce7004432437906bb1ae45dcba93f7146d1b42f7
root <5761@holbertonstudents.com>
2022-10-31 08:32:32 -0700

    asd

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: pointer to the function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
