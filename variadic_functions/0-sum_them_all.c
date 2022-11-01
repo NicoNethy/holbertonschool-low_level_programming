de818839814926ca34b0b81b9c12b28de5b42272
root <5761@holbertonstudents.com>
2022-11-01 16:42:00 -0700

    asd

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all its parameters.
+ * @n: inte to add
+ * Return: 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0, i;
+
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+
+	return (sum);
+}
