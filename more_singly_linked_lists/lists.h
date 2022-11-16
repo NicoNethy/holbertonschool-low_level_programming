aa9e813e96bb0f49fa094e7d1e08866ba19a335f
root <5761@holbertonstudents.com>
2022-11-16 07:50:39 -0800

    asd

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -190,10 +190,11 @@ void free_list(list_t *head);
  * @next: points to the next node
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
-		    int n;
-		    	    struct listint_s *next;
+	int n;
+		    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
