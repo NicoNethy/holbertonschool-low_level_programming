a81792353f5739ec7c842d3ce2e990e1e27d969b
root <5761@holbertonstudents.com>
2022-11-19 19:42:48 -0800

    asd

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,6 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
 #include <string.h>
 #include <stdio.h>
 
