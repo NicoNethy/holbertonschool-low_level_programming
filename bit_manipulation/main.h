8316a56762ac17b71063cd688097616322581779
root <5761@holbertonstudents.com>
2022-11-20 15:43:34 -0800

    asd

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -190,7 +190,7 @@ void free_list(list_t *head);
 typedef struct listint_s
 {
 		    int n;
-		    	    struct listint_s *next;
+		    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
