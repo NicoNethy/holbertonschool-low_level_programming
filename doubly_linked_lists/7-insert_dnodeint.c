c4b2ff43811b338708f261017d23216fc7056c6f
root <5761@holbertonstudents.com>
2022-11-27 14:56:29 -0800

    asd

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = tmp;
 	new->next = tmp->next;
 	tmp->next->prev = new;
-	tmp->next = new;	
+	tmp->next = new;
 
 	return (new);
 }
