03448ff6b803d182eb115acef39150bcae5b4cac
root <5761@holbertonstudents.com>
2023-02-15 11:00:13 -0800

    asd

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+/**
+ * main - start
+ * Return: allways
+ */
 
 int main(void)
 {
 	int a;
 
-	for(a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 	putchar(a);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
