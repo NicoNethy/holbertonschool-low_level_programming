09c1e8081fa412be661fb585cb80f04942ca48ec
root <5761@holbertonstudents.com>
2023-02-15 13:23:33 -0800

    asd

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,11 +5,11 @@
  */
 
 int _islower(int c)
-{	 
+{
 	if (c >= 65 && c <= 90)
-		return(0);
+		return (0);
 
 	else if (c >= 97 && c <= 122)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
