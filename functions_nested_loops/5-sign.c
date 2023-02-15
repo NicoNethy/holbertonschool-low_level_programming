53962b226a4a0ccbb813c182ceeff761276651e9
root <5761@holbertonstudents.com>
2023-02-15 14:15:39 -0800

    asd

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sing - the function
+ * @n: integer
+ * Return: allways
+ */
+
+int print_sign(int n)
+{
+	if (n >= '0')
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 48)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	return (0);
+}
+
