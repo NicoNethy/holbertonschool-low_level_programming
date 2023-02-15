3b4e2272dd2f719fc46e59eae6f62109eadcf204
root <5761@holbertonstudents.com>
2023-02-15 13:20:20 -0800

    asd

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function
+ */
+
+int _islower(int c)
+{	 
+	if (c >= 65 && c <= 90)
+		return(0);
+
+	else if (c >= 97 && c <= 122)
+		return(1);
+	return(0);
+}
