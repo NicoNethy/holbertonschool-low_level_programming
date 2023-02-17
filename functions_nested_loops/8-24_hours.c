72c8401d896c26919de42a0aa25266e03a68c05b
root <5761@holbertonstudents.com>
2023-02-17 03:21:02 -0800

    venticuatro

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the hours and the minutes
+ */
+
+void jack_bauer(void)
+
+{
+	int hor;
+	int min;
+
+	for (hor = 0; hor < 24; hor++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hor / 10) + '0');
+			_putchar((hor % 10) + '0');	
+			_putchar(':');			
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
