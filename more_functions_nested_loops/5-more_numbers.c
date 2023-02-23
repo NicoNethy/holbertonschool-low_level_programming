e145dcfc8e0857dec266c9e544324366f5b4240c
root <5761@holbertonstudents.com>
2023-02-23 07:41:38 -0800

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *more_numbers - the function
  */
 
 void more_numbers(void)
@@ -13,9 +11,9 @@ void more_numbers(void)
 
 	for (n = 48; n <= 57; n++)
 	{
-		for(i = 0; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			if(i >= 10)
+			if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
 			}
