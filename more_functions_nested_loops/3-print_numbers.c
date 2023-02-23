8cfb49c75ddb23e803b19fae324dc2f3d5d830a1
root <5761@holbertonstudents.com>
2023-02-23 04:33:31 -0800

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,13 @@
 
 /**
  *print_numbers - the function
- *@n: int
- *Return: allways
+ *Return: void
  */
 
 void print_numbers(void)
 {
 	int n;
 
-	for (n = 48, n <= 57, n++)
+	for (n = 48; n <= 57; n++)
 		_putchar(n);
-	return(0);
 }
