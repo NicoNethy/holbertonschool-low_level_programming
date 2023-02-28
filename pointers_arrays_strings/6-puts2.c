17a7a58ad5072b22dfa817dffe3d65f32d12f0c7
root <5761@holbertonstudents.com>
2023-02-28 03:35:31 -0800

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - the function
+ *@str: the string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar(10);
+}
