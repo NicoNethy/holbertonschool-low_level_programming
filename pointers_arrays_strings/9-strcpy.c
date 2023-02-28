2984a09e4303a3d84a64ca053c2ed992ca3abdfa
root <5761@holbertonstudents.com>
2023-02-28 05:30:18 -0800

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcpy - bababooey
+ *@dest: destinatrion
+ *@stc: sorce
+ *Return: allways
+ */
+
+char *_strcpy(char *dest, char *src)
+
+
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
