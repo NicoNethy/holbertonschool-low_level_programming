015a3083a1202382bdc3c94dc3a2827224fb6776
root <5761@holbertonstudents.com>
2023-03-06 09:00:55 -0800

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *factorial - the function
+ *Return: a lot of things
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
