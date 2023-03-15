f13cf77464e1c5e86f06ae16353b4bb57e1c3559
root <5761@holbertonstudents.com>
2023-03-15 06:20:22 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strdup - the function
+ *@str: la string
+ *Return: coca
+ */
+
+char *_strdup(char *str)
+{
+	int a;
+	char *coca;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		;
+	}
+       	coca = malloc(sizeof(char) * a + 1);
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		coca[a] = str[a];
+	}
+
+	return (coca);
+}
