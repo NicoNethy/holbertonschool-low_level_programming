b17a06e8ceb9a7e2e22ed7427797182d828ea753
root <5761@holbertonstudents.com>
2023-03-15 04:28:32 -0700

    asd

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - the function
+ *@argc: the arguments
+ *@argv: the string
+ *Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	(void) argc;
+	for (a = 1; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
