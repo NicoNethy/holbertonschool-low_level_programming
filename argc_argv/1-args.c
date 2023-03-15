2213841f78459d7dd3b2190aeb4160f240b5ac02
root <5761@holbertonstudents.com>
2023-03-15 04:12:17 -0700

    coca

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *main - the function
+ *@argc: the arguments
+ *@argv: the string
+ *Return: zero
  */
 
-
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	argc = argc - 1;
+	printf("%d\n", argc);
+	return (0);
+}
