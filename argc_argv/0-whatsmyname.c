96367ef38501370f51b67bb664164872ac3f500f
root <5761@holbertonstudents.com>
2023-03-15 04:04:19 -0700

    this

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - the function
+ *@argc: the void
+ *@argv: the string
+ *Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
