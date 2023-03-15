add6409c2d907ef078a25fdd8898418278752734
root <5761@holbertonstudents.com>
2023-03-15 06:45:30 -0700

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ *str_concat - the function
+ *@s1: la string uno
+ *@s2: la string dos
+ *Return: coca
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a;
+	int b;
+	char *coca;
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		;
+	}
+
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+		;
+	}
+
+	coca = malloc(sizeof(char) * (a + b) + 1);
+
+	if (coca == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		coca[a] = s1[a];
+	}
+	for (b = 0; s2[b] != '\0'; b++, a++)
+	{
+		coca[a] = s2[b];
+	}
+	coca[a] = '\0';
+	return (coca);
+}
