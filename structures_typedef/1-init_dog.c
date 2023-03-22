3645bfad81191f6a9e1caccd59dccf3525af39cc
root <5761@holbertonstudents.com>
2023-03-22 14:28:35 -0700

    coca

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 
+/**
+ *init_dog - the function that initiates the dog
+ *@d: the pointer of the dog
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the name of the owner
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
