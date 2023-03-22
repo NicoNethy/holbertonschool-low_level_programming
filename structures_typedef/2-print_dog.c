049d7c8695d4c0373a848b48ecd729bcfe69053a
root <5761@holbertonstudents.com>
2023-03-22 15:06:28 -0700

    orint

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,40 @@
 #include "dog.h"
+#include <stdlib.h>
+/**
+ *print_dog - Prints struct
+ *@d: elements
+ *
+ */
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
