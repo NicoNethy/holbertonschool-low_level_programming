c158e4ef83c0f67c7b240826de921c74470c2391
root <5761@holbertonstudents.com>
2023-03-23 03:27:47 -0700

    fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * array_iterator - executes a function given as a parameter
  * @array: array
  * @size: the size of the array
  * @action: pointer to the function
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
+
 {
 	size_t a = 0;
 
