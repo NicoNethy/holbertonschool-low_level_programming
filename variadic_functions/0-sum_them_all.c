3b1ed17c401116bb3fbfaedaa43e603af60da0a4
root <5761@holbertonstudents.com>
2023-03-23 04:26:49 -0700

    finally

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,8 +2,9 @@
 #include <stdarg.h>
 
 /**
- *
- *
+ *sum_them_all - the function
+ *@n: the quantity of numbers
+ *Return: the sum
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -12,19 +13,18 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int a;
 	int b;
 
-	va_start (coca, n);
-	
-	 if (n == 0)
-	 {
-		 return (0);
-	 }
+	va_start(coca, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
 
 	b = 0;
 	for (a = 0; a < n; a++)
 	{
-		b += va_arg (coca, int);
+		b += va_arg(coca, int);
 	}
-	va_end (coca);
+	va_end(coca);
 	return (b);
-
 }
