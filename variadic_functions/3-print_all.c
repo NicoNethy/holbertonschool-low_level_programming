b9bd0fa47393f66e1f45a58f16f18d06d2547bf7
root <5761@holbertonstudents.com>
2023-03-26 05:58:15 -0700

    el printo ol

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,81 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+#include<stdarg.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_all(const char * const format, ...)
+
+{
+	va_list list;
+	int i = 0, l = 0, a = 0;
+	char c = '\0';
+	float f = 0.0;
+	char *s = NULL;
+
+	va_start(list, format);
+	while (format && format[i])
+	{
+		a = 0;
+		switch (format[i])
+		{
+			case 'c':
+				c = va_arg(list, int);
+				a = printInt(c);
+				break;
+			case 'i':
+				l = va_arg(list, int);
+				a = printChar(l);
+				break;
+			case 'f':
+				f = va_arg(list, double);
+				a = printFloat(f);
+				break;
+			case 's':
+				s = va_arg(list, char *);
+				if (s == NULL)
+					s = "(nil)";
+				a = printString(s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1] != '\0' && a == 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
+
+	int
+printInt(int b)
+{
+	printf("%c", b);
+	return (1);
+}
+	int
+printChar(int b)
+{
+	printf("%i", b);
+	return (1);
+}
+	int
+printFloat(double b)
+{
+	printf("%f", b);
+	return (1);
+}
+	int
+printString(char *b)
+{
+	printf("%s", b);
+	return (1);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,18 +4,14 @@
 #include<stdio.h>
 #include<stdarg.h>
 
-	/*task 0*/
 int sum_them_all(const unsigned int n, ...);
-
-	/*task 1*/
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-	/*task 2*/
+void print_numbers(const char *separator, const unsigned int n, ...);	
 void print_strings(const char *separator, const unsigned int n, ...);
-
-	/*task 3*/
 void print_all(const char * const format, ...);
-
-int _putchar(char);
+int _putchar(char);;
+int printInt(int b);
+int printChar(int b);
+int printFloat(double b);
+int printString(char *b);
 
 #endif
