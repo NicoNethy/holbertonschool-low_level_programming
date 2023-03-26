c531926d1a78239a91e3ace3a84078d5906e0f6e
root <5761@holbertonstudents.com>
2023-03-26 05:37:39 -0700

    la calculadora 1

---
+++ function_pointers/3-main.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+
+
+int main(int argc, char *argv[])
+
+{
+	int (*oper)(int, int);
+
+	if (argc == 4)
+	{
+		oper = get_op_func(argv[2]);
+		printf("%d\n", oper(atoi(argv[1]), atoi(argv[3])));
+		return (0);	
+	}
+	printf("Error\n");
+	exit(98);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ * op_add - the function
+ * @a: an int
+ * @b: an int
+ * Return: allways
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - the function
+ * @a: an int
+ * @b: an int
+ * Return: allways
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - the function
+ * @a: an int
+ * @b: an int
+ * Return: allways
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - the function
+ * @a: an int
+ * @b: an int
+ * Return: allways
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		return (100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - the function
+ * @a: an int
+ * @b: an int
+ * Return: allways
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return (100);
+	}
+	return (a % b);
+}
