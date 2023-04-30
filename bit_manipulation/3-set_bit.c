48e0806d11f565b5d5ad5249a93a85660ab9732b
root <5761@holbertonstudents.com>
2023-04-30 08:44:44 -0700

    Some more

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * get_bit - Funct to get ind
+ * set_bit - Func to set it
  * @n: int
  * @index: the index
  * Return: allways
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 32)
 		return (-1);
-
-	return (n >> index & 1);
+	*n |= 1 << index;
+	return (1);
 }
