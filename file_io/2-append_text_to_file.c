9e7b2f80ed71cd0a9124e2e46b8b63212a793dde
root <5761@holbertonstudents.com>
2023-05-04 23:22:49 -0700

    374

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append funct
+ * @filename: the naem of the file
+ * @text_content: the content
+ * Return: if works 1, if not, not
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int	file2;
+
+	if (filename == NULL || !*filename)
+	{
+		return (-1);
+	}
+	
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (cont = 0; *(text_content + file2; file2++)
+			;
+		write(file, text_content, file2);
+	}
+	close(file)
+		return (1);
+}
