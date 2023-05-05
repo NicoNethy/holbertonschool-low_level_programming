bf46e9ffda300df0a496cb732a137416a6780e8d
root <5761@holbertonstudents.com>
2023-05-04 23:15:50 -0700

    2/4

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,7 +25,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (f2 = 0; text_content[f2]; f2++);
+	for (f2 = 0; text_content[f2]; f2++)
+		;
 	if (f2 != 0)
 		write(file, text_content, f2);
 	close(file);
